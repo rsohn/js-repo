ba10271769ec7981b9fbd3b54a0d89179ab716fe
rsohn <richard.sohn@alcatel-lucent.com>
2016-06-30 18:48:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello from Cisco Shipped!');
+    res.send('Hello from Cisco Shipped! <p>Thanks for learning more about us!</p>');
 });
 
 var server = app.listen(3000, function () {
